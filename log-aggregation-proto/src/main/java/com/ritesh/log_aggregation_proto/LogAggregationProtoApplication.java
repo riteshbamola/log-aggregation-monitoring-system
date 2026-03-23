b1a782212d8fb27ac0925999f0475c98ac9ca413
Ritesh Bamola <rishubamola01@gmail.com>
2026-03-23 23:27:45 +0530

    Proto file for Log

---
+++ log-aggregation-proto/src/main/java/com/ritesh/log_aggregation_proto/LogAggregationProtoApplication.java
@@ -0,0 +1,13 @@
+package com.ritesh.log_aggregation_proto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAggregationProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAggregationProtoApplication.class, args);
+	}
+
+}
